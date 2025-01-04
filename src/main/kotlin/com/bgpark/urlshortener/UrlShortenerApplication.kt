5d9733b13efd83e456177b3fd716eb999b9e0685
Peter Park <peter.park@zalando.de>
2025-01-04 11:45:15 +0100

    feat: add base time entity

--- src/main/kotlin/com/bgpark/urlshortener/UrlShortenerApplication.kt
+++ src/main/kotlin/com/bgpark/urlshortener/UrlShortenerApplication.kt
@@ -2,8 +2,10 @@ package com.bgpark.urlshortener
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class UrlShortenerApplication
 
 fun main(args: Array<String>) {
