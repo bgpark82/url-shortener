8d2485c6038e938109b63291300c87c1bd1b53cc
Peter Park <peter.park@zalando.de>
2025-01-05 16:37:50 +0100

    refactor: remove unused method

--- src/main/kotlin/com/bgpark/urlshortener/domain/Url.kt
+++ src/main/kotlin/com/bgpark/urlshortener/domain/Url.kt
@@ -31,29 +31,9 @@ class Url(
         shortUrl = ""
     )
 
-    /**
-     * Generates a shortened URL by appending the given hash to the base URL.
-     *
-     * @param hash The unique identifier (hash) for the shortened URL.
-     * @see BASE_URL The base URL to which the hash will be appended.
-     */
-    fun addShortUrl(baseUrl: String, hash: String) {
-        validateUrl(baseUrl)
-        validateHash(hash)
-
-        val cleanedBaseUrl = baseUrl.trim().removeSuffix("/") // Remove trailing slash if exists
-        this.shortUrl = "$cleanedBaseUrl/${hash.trim()}"
-    }
-
-    private fun validateUrl(baseUrl: String) {
-        if (baseUrl.isBlank()) {
-            throw ApplicationException(ErrorCode.EMPTY_OR_WHITESPACE_INPUT, FieldError(field = "baseUrl", value = baseUrl))
-        }
-    }
-
-    private fun validateHash(hash: String) {
-        if (hash.isBlank()) {
-            throw ApplicationException(ErrorCode.EMPTY_OR_WHITESPACE_INPUT, FieldError(field = "hash", value = hash))
-        }
-    }
+    constructor(longUrl: String, shortUrl: String): this(
+        id = 0L,
+        longUrl = longUrl,
+        shortUrl = shortUrl
+    )
 }
